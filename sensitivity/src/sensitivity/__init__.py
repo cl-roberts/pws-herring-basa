7bc62744f1eef80d67dab99b42267acc86953fb0
CL Roberts <cl.roberts@alaska.gov>
2024-11-29 12:45:07 -0800

    begin writing py modules

---
+++ sensitivity/src/sensitivity/__init__.py
@@ -0,0 +1,1 @@
+from sensitivity import run_basa
---
+++ sensitivity/src/sensitivity/run_basa.py
@@ -0,0 +1,14 @@
+"""
+Execute BASA
+"""
+
+def knn_regression(dir_model): 
+    """
+    This is a wrapper function for running the Bayesian age-structured assessment
+    (BASA) model. It simply is a python wrapper for calling an R script which 
+    runs the model.
+
+    :param str dir_model: Path to run_basa.r script
+    """
+
+    return()
