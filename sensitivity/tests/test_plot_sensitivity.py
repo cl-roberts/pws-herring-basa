2bfebfe76b46bda9680561340a90c40d8156048e
TEHinshaw <thinshaw27@gmail.com>
2024-12-03 12:30:48 -0800

    Fixing typos and adding Qs to track

--- sensitivity/tests/test_plot_sensitivity.py
+++ sensitivity/tests/test_plot_sensitivity.py
@@ -1,8 +1,17 @@
 import os
 from PIL import Image
 
+# Test Data:
+sensitivity_testdata = {"Years": [2010,2011,2012, 2013],
+                    "Median Pre-fishery biomass (in 1000s metric tons)": [100, 120, 130, 140]}
+
+base_testdata = {"Years": [2010,2011,2012, 2013],
+                    "Median Pre-fishery biomass (in 1000s metric tons)": [90, 110, 150, 130]}
+
 # Smoke test: make sure the code actually runs
 def test_smoke_plotsensivity():
+    # still need inputs of course
+    # Q: should I use provided test data or should I use the package tempfiles so tests are easily run without inputs?  
     plot_sensitivity()
 
 def test_plotcreated_plotsensitivity():
@@ -11,10 +20,10 @@ def test_plotcreated_plotsensitivity():
     # some code here (test inputs or something)
 
     try:
-        plot_sensitivity(directory, output_path)
+        plot_sensitivity(input_directory, output_path)
 
         # check that file exists
-        assert os.path.exists(output_path), "Plot file was no created"
+        assert os.path.exists(output_path), "Plot file was not created"
 
         # opens the file to verify its a valid PNG
         with Image.open(output_path) as image:
@@ -31,7 +40,9 @@ def test_plotcreated_plotsensitivity():
 
 # Edge tests: check to make sure that errors are being caught
 def test_filenotfound_plotsensitivity():
-    # csv file does note exist so check to make sure its caught
+    # csv file does not exist so check to make sure its caught
 
 def test_wrongcolumns_plotsensitivity():
-    # data with wrong columns... (ie no biomass or years)
+    # data with wrong columns... (ie no biomass or years columns)
+
+#FUTURE DESIRED TEST: Test that verfies the resulting plot is what the plot should be (ie expected output = output)
