4ab216cbe49a1bf30f76dc099ed8cc782514c556
TEHinshaw <thinshaw27@gmail.com>
2024-12-03 09:20:24 -0800

    Made file more pylint friendly, fixed error in try and except, fixed missing paren., fixed ValueError message

--- sensitivity/src/sensitivity/plot_sensitivity.py
+++ sensitivity/src/sensitivity/plot_sensitivity.py
@@ -6,7 +6,7 @@ def plot_sensitivity(directory, output_path):
     Plots the sensitvity model biomass time series alongsize the base model.
     
     Parameters:
-    directory: string, directory containing the sensitivty and outputs
+    directory: string, directory containing the sensitivity and outputs
     output_path: string, the file path to save the plot
 
     Return:
@@ -22,24 +22,24 @@ def plot_sensitivity(directory, output_path):
         sensitivity_data = pd.read_csv(sensitivity_file)
         base_data = pd.read_csv(base_file)
     except FileNotFoundError as err:
-        raise FileNotFoundError(f"Could not find file: {err.filename}")
-    
+        raise FileNotFoundError(f"Could not find file: {err.filename}") from err
+
     # Verifying necessary columns are present (I'm assuming year ?)
     if not {"Years", "Median Pre-fishery biomass (in 1000s metric tons)"}.issubset(sensitivity_data.columns):
-        raise ValueError("Sensitivity data must have 'year' and 'biomass' columns.")
+        raise ValueError("Sensitivity data must have 'Years' and 'Median Pre-fishery biomass (in 1000s metric tons)' columns.")
     if not {"Years", "Median Pre-fishery biomass (in 1000s metric tons)"}.issubset(base_data.columns):
-        raise ValueError("Base data must have 'year' and 'biomass' coulmns.")
+        raise ValueError("Base data must have 'Years' and 'biomaMedian Pre-fishery biomass (in 1000s metric tons)ss' coulmns.")
 
     # Plots
     plt.figure(figsize=(10,6))
-    plt.plot(sensitivity_data["Years"], 
-             sensitivity_data["Median Pre-fishery biomass (in 1000s metric tons)"], 
+    plt.plot(sensitivity_data["Years"],
+             sensitivity_data["Median Pre-fishery biomass (in 1000s metric tons)"],
              label = "Sensitivity Model",
              linestyle = "-",
              color = "blue")
-    
-    plt.plot(sensitivity_data["Years"], 
-             sensitivity_data["Median Pre-fishery biomass (in 1000s metric tons)"], 
+
+    plt.plot(sensitivity_data["Years"],
+             sensitivity_data["Median Pre-fishery biomass (in 1000s metric tons)"],
              label = "Base Model",
              linestyle = "--",
              color = "orange")
@@ -47,7 +47,7 @@ def plot_sensitivity(directory, output_path):
     plt.xlabel("Years")
     plt.ylabel("Median Biomass")
     plt.title("Biomass Time Series: Sensitivity vs Base Models")
-    plt.legend
+    plt.legend()
     plt.grid(True)
 
     plt.savefig(output_path, dpi=300)
