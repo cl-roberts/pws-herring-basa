7bc49846ec0d5f69bc31070af7e9578988eab48e
vlee216 <nerdyvlee@gmail.com>
2024-12-02 22:48:28 -0800

    "first draft for plot_sensitivity_comparison module"

--- sensitivity/src/sensitivity/plot_sensitivity_comparison.py
+++ sensitivity/src/sensitivity/plot_sensitivity_comparison.py
@@ -1,5 +1,23 @@
 """This module defines a function for creating a plot of error metrics"""
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def plot_sens_comparison():
-    """Read a csv file and return a plot"""
-    
+def plot_sens_comparison(directory):
+    """This function reads a csv file and return a plot"""
+    # Read in the csv file
+    perc_df = pd.read_csv(directory)
+    # Drop the first column (unnecessary index) if present
+    if perc_df.shape[1] == 3:
+        perc_df = perc_df.drop(perc_df.columns[0], axis = 1)
+    # Create plot
+    plt.figure(figsize = (10,6))
+    plt.plot(perc_df["Year"], perc_df["Percentage Error"])
+    plt.xlabel("Year")
+    plt.ylabel("Percentage Error")
+    plt.title("Percentage Error of Biomass: Sensitivity vs Base Model")
+    plt.legend
+    return plt.gcf()
+    plt.show()
+
+
+# To test, use directory_path "../../data_outputs/sensitivity_comparison.csv"
