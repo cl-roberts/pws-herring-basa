6e8be4dcf9aefa31cfad488761ea78a6c48584fb
TEHinshaw <thinshaw27@gmail.com>
2024-12-03 09:46:49 -0800

    Starting to add detailed code to plot creation test

--- sensitivity/tests/test_plot_sensitivity.py
+++ sensitivity/tests/test_plot_sensitivity.py
@@ -1,3 +1,4 @@
+import os
 from PIL import Image
 
 # Smoke test: make sure the code actually runs
@@ -7,6 +8,27 @@ def test_smoke_plotsensivity():
 def test_plotcreated_plotsensitivity():
     # verify a plot is created
     
+    # some code here (test inputs or something)
+
+    try:
+        plot_sensitivity(directory, output_path)
+
+        # check that file exists
+        assert os.path.exists(output_path), "Plot file was no created"
+
+        # opens the file to verify its a valid PNG
+        with Image.open(output_path) as image:
+            image.verify()
+            assert image.format == "PNG", "Plot file is not in PNG format"
+            print("Test passed: plot created successfully and is a PNG image")
+
+    except Exception as e:
+        print (f"Test failed: {e}")
+
+    finally: # cleanup and remove test output
+        if os.path.exists(output_path):
+            os.remove(output_path)
+
 # Edge tests: check to make sure that errors are being caught
 def test_filenotfound_plotsensitivity():
     # csv file does note exist so check to make sure its caught
