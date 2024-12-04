917cc7d8ccb6e4d24e56307b98c4f8e92a3305a3
mindydai <mdai2@berkeley.edu>
2024-12-04 02:40:26 -0800

    edited code for 10/10 in pylint

--- sensitivity/src/sensitivity/run_basa.py
+++ sensitivity/src/sensitivity/run_basa.py
@@ -4,7 +4,7 @@ Execute BASA
 
 import subprocess
 
-def run_basa(dir_sensitivity): 
+def run_basa(dir_sensitivity):
     """
     This is a wrapper function for running the Bayesian age-structured assessment
     (BASA) model. It simply is a python wrapper for calling an R script which 
@@ -16,11 +16,11 @@ def run_basa(dir_sensitivity):
     cmd_run_basa = "Rscript " + dir_sensitivity + "/run_basa.r"
     cmd_save_outputs = "Rscript " + dir_sensitivity + "/plotting/plot_management_outputs.R"
 
-    try: 
+    try:
         subprocess.check_output(cmd_run_basa)
         subprocess.call(cmd_save_outputs)
     except subprocess.CalledProcessError:
         print("Model did not converge, try another value for M")
-        return(1)
+        return 1
 
-    return(0)
+    return 0
