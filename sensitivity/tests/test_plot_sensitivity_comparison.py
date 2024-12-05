4c75a8f30150130b4387df5cf7151ff5c7d20048
vlee216 <nerdyvlee@gmail.com>
2024-12-04 21:38:23 -0800

    "Added grid to module 6 plot; fixed plot naming convention"

--- sensitivity/tests/test_plot_sensitivity_comparison.py
+++ sensitivity/tests/test_plot_sensitivity_comparison.py
@@ -1,9 +1,6 @@
 """This is a test module for plot_sensitivity_comparison.py"""
 import pandas as pd
+import matplotlib.pyplot as plt
 import pytest
-from sensitivity_comparison import sc
+from plot_sensitivity_comparison import psc
 
-# Change this to a smoke test -- just to see that the function runs
-# Randomness means that a one-shot test won't work
-def test_oneshot():
-    round(sc.sens_comparison("../../data_outputs/outputs-for-management.csv").iloc[0,0], 10) == 13.0621471667
