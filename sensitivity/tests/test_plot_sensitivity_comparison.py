9d2db1f69d2db3c1f09542851d92504b33251392
vlee216 <nerdyvlee@gmail.com>
2024-12-05 00:35:30 -0800

    Forgot to add name for plot in the previous commit; fixed now

--- sensitivity/tests/test_plot_sensitivity_comparison.py
+++ sensitivity/tests/test_plot_sensitivity_comparison.py
@@ -7,7 +7,7 @@ import plot_sensitivity_comparison as psc
 
 def test_smoke_psc():
     """This is an smoke test for the sens_comparison function"""
-    psc.plot_sens_comparison("../data_outputs/", "../data_outputs/")
+    psc.plot_sens_comparison("../data_outputs/", "../data_outputs/sensitivity_comparison_plot")
 
 # In the future, should add edge tests for invalid data paths or issues with image generation
 
