b333cdf8de5217a6eefe0be1791e738b3f745361
vlee216 <nerdyvlee@gmail.com>
2024-12-05 00:34:03 -0800

    "Wrote smoke test for module 6"

--- sensitivity/tests/test_plot_sensitivity_comparison.py
+++ sensitivity/tests/test_plot_sensitivity_comparison.py
@@ -1,6 +1,14 @@
 """This is a test module for plot_sensitivity_comparison.py"""
-import pandas as pd
-import matplotlib.pyplot as plt
-import pytest
-from plot_sensitivity_comparison import psc
+import sys
+sys.path.append('../src/sensitivity')
+import plot_sensitivity_comparison as psc
 
+# Created a smoke test to see if the function runs
+
+def test_smoke_psc():
+    """This is an smoke test for the sens_comparison function"""
+    psc.plot_sens_comparison("../data_outputs/", "../data_outputs/")
+
+# In the future, should add edge tests for invalid data paths or issues with image generation
+
+# For this test, run the pytest commmand line prompt in sensitivity\tests
