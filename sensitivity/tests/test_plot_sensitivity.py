9504197ce5deaecb21ee0ea0c2d8b768d89a6224
TEHinshaw <thinshaw27@gmail.com>
2024-12-05 08:36:49 -0800

    Cleaned up path_sensitivity test file and completed first draft of the smoke test

--- sensitivity/tests/test_plot_sensitivity.py
+++ sensitivity/tests/test_plot_sensitivity.py
@@ -1,19 +1,38 @@
 import os
+import tempfile
 from PIL import Image
+import pandas as pd
+from plot_sensitivity import plot_sensitivity
 
 # Test Data:
-sensitivity_testdata = {"Years": [2010,2011,2012, 2013],
-                    "Median Pre-fishery biomass (in 1000s metric tons)": [100, 120, 130, 140]}
+sensitivity_testdata = {
+    "Years": [2010,2011,2012, 2013],
+    "Median Pre-fishery biomass (in 1000s metric tons)": [100, 120, 130, 140]
+    }
 
-base_testdata = {"Years": [2010,2011,2012, 2013],
-                    "Median Pre-fishery biomass (in 1000s metric tons)": [90, 110, 150, 130]}
+base_testdata = {
+    "Years": [2010,2011,2012, 2013],
+    "Median Pre-fishery biomass (in 1000s metric tons)": [90, 110, 150, 130]
+    }
 
 # Smoke test: make sure the code actually runs
 def test_smoke_plotsensivity():
-    # still need inputs of course
-    # Q: should I use provided test data or should I use the package tempfiles so tests are easily run without inputs?  
-    plot_sensitivity()
+    with tempfile.TemporaryDirectory() as tempdir:
+        sensitivity_testfile = os.path.join(tempdir, "outputs-for-management.csv")
+        base_testfile = os.path.join(temp_dir, "outputs-for-management-base.csv")
 
+        pd.DataFrame(sensitivity_testdata, index = False)
+        pd.DataFrame(base_testdata, index = False)
+
+        output_path = os.path.join(tempdir, "test_plot.png")
+
+        try:
+            plot_sensitivity(tempdir, output_path)
+            print("Smoke test passed!")
+        except Exception as e:
+            raise AssertionError(f"Smoke test failed: {e}")
+        
+# Test: Verify a plot is created and is valid
 def test_plotcreated_plotsensitivity():
     # verify a plot is created
     
@@ -38,11 +57,13 @@ def test_plotcreated_plotsensitivity():
         if os.path.exists(output_path):
             os.remove(output_path)
 
+#FUTURE DESIRED TEST: 
+
 # Edge tests: check to make sure that errors are being caught
-def test_filenotfound_plotsensitivity():
+# def test_filenotfound_plotsensitivity():
     # csv file does not exist so check to make sure its caught
 
-def test_wrongcolumns_plotsensitivity():
+# def test_wrongcolumns_plotsensitivity():
     # data with wrong columns... (ie no biomass or years columns)
 
-#FUTURE DESIRED TEST: Test that verfies the resulting plot is what the plot should be (ie expected output = output)
+# Test that verfies the resulting plot is what the plot should be (ie expected output = output)
